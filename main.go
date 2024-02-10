018feeb4aeee3d95158f994e28ad72be9b6c40bc
jc <46619361+juancwu@users.noreply.github.com>
2024-02-10 11:00:41 -0500

    adding views

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/juancwu/jellyfan-web/views/page"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Jellyfan Web")
+
+	e := echo.New()
+
+	e.Static("/static", "static")
+
+	e.GET("/", func(c echo.Context) error {
+		page.LandingPage().Render(context.Background(), c.Response().Writer)
+		return nil
+	})
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
 }
