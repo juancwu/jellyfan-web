c2f4a18523f9ad3817f9219f26e026ed5d3c83b1
jc <46619361+juancwu@users.noreply.github.com>
2024-02-11 23:03:49 -0500

    rename util/fs.go to util/util.go

--- util/util.go
+++ util/util.go
@@ -3,6 +3,6 @@ package util
 import "path/filepath"
 
 func FileNameWithExt(filename string) bool {
-    fileExt := filepath.Ext(filename)
-    return fileExt != ""
+	fileExt := filepath.Ext(filename)
+	return fileExt != ""
 }
